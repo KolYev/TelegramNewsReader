e45f3b02607faea1c557c1a194c3296a687f0f77
KolYev <evgeniy.kolesenko@gmail.com>
2026-05-17 14:56:59 +0300

    main.py: получаем данные аккаунта

---
+++ main.py
@@ -0,0 +1,10 @@
+from telethon.sync import TelegramClient
+
+import csv
+
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+
+api_id = int(input("Введите api_id вашего аккаунта: "))
+api_hash = str(input("Введите api_hash вашего аккаунта: "))
+phone = str(input("Введите номер телефона, привязанный к вашему аккаунту: "))
