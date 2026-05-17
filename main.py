35d77bca80e49e8d2545f5770c19ac7bd64a52e7
KolYev <evgeniy.kolesenko@gmail.com>
2026-05-17 15:01:38 +0300

    main.py: запускаем клиента

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ from telethon.tl.types import InputPeerEmpty
 api_id = int(input("Введите api_id вашего аккаунта: "))
 api_hash = str(input("Введите api_hash вашего аккаунта: "))
 phone = str(input("Введите номер телефона, привязанный к вашему аккаунту: "))
+
+client = TelegramClient(phone, api_id, api_hash)
+client.start()
