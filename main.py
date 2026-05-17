0b86115ba6ba92c38c8a17b44f89d9e38ba95e1f
KolYev <evgeniy.kolesenko@gmail.com>
2026-05-17 15:49:59 +0300

    main.py: чтение всех чатов за последний день

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient
 
-import csv
+from datetime import datetime, timedelta, timezone
 
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
@@ -9,32 +9,30 @@ api_id = int(input("Введите api_id вашего аккаунта: "))
 api_hash = str(input("Введите api_hash вашего аккаунта: "))
 phone = str(input("Введите номер телефона, привязанный к вашему аккаунту: "))
 
-client = TelegramClient(phone, api_id, api_hash)
-client.start()
+# получаем текущее время
+now = datetime.now(timezone.utc)
 
-chats = []
-last_date = None
-size_chats = 200
-groups=[]
+# сбрасываем время на начало дня
+time_threshold = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
-result = client(GetDialogsRequest(
-            offset_date=last_date,
-            offset_id=0,
-            offset_peer=InputPeerEmpty(),
-            limit=size_chats,
-            hash = 0
-        ))
-chats.extend(result.chats)
-
-for chat in chats:
-   try:
-       if chat.megagroup== True:
-           groups.append(chat)
-   except:
-       continue
-   
-print('Список каналов:')
-i=0
-for g in groups:
-   print(str(i) + '- ' + g.title)
-   i+=1
+with TelegramClient(phone, api_id, api_hash) as client:
+    # Создаём файл для записи новостей за последний день
+    with open("today_news.txt", "w", encoding="utf-8") as file:
+        
+        print("Получаем список каналов...")
+        # Получаем все диалоги пользователя
+        for dialog in client.get_dialogs():
+            if dialog.is_channel:
+                print(f"Сканирую канал: {dialog.name}")
+                file.write(f"\n{'='*20} КАНАЛ: {dialog.name} {'='*20}\n\n")
+                
+                # Цикл сообщений от старых к новым
+                for message in client.iter_messages(dialog):
+                    # Если сообщение создано раньше чем вчера, то мы останавливаемся
+                    if message.date < time_threshold:
+                        break
+                    
+                    if message.text:
+                        msg_time = message.date.strftime('%Y-%m-%d %H:%M:%S')
+                        file.write(f"[{msg_time}] {message.text}\n")
+                        file.write("-" * 50 + "\n")
